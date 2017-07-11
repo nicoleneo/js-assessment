483b16c645ad608a45db7567851316a3b90079e1
Nicole Neo <nicolen@gmail.com>
2017-07-11 12:02:46 +0100

    Async and best practices

--- app/async.js
+++ app/async.js
@@ -2,10 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+  	return new Promise(function(resolve, reject) {
+  		setTimeout(function() {
+  			resolve(value);
+  	}, 10);
+  	});
   },
 
   manipulateRemoteData: function(url) {
-
+  	return new Promise(function(resolve, reject) {
+  		$.get(url)
+  		.done(function(result) {
+  			var people = result.people;
+  			var sorted = people.map(function(val) {
+  				return val.name;
+  			}).sort();
+  			resolve(sorted);
+  		});
+  	});
   }
 };
